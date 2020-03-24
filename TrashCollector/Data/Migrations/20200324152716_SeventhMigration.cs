4f372c7a68d606aca5ce7d0fdaaa79ad53495cb6
Dylan DeRouchie <dylan.f.derouchie@gmail.com>
2020-03-24 14:25:39 -0500

    did a migration after adding some props, still working on the employee controller and views.

---
+++ TrashCollector/Data/Migrations/20200324152716_SeventhMigration.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TrashCollector.Data.Migrations
+{
+    public partial class SeventhMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "IdentityUserId",
+                table: "Employee",
+                nullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "1",
+                column: "ConcurrencyStamp",
+                value: "0c2a3a82-2467-4efa-9461-f8d73d961dfe");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "2",
+                column: "ConcurrencyStamp",
+                value: "fa7b3536-1a74-4ac9-9d03-597d575b9864");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Employee_IdentityUserId",
+                table: "Employee",
+                column: "IdentityUserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Employee_AspNetUsers_IdentityUserId",
+                table: "Employee",
+                column: "IdentityUserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Employee_AspNetUsers_IdentityUserId",
+                table: "Employee");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Employee_IdentityUserId",
+                table: "Employee");
+
+            migrationBuilder.DropColumn(
+                name: "IdentityUserId",
+                table: "Employee");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "1",
+                column: "ConcurrencyStamp",
+                value: "3860b57e-ad72-4070-994b-70ca0107cc4a");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "2",
+                column: "ConcurrencyStamp",
+                value: "515fc9b2-e9fa-4094-8a02-bdf6c873ed5e");
+        }
+    }
+}
